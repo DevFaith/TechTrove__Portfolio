b687408cf88057ed203250423641d078587dca24
DevFaith <waitherafaith@outlook.com>
2024-06-20 10:03:05 +0300

    * style(Footer.jsx): reformat code for better readability and consistency * feat(Footer.jsx): update footer layout and add social media icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import { FaDev } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaGithubSquare } from "react-icons/fa";
-import { FaGitlab } from "react-icons/fa";
+import React from 'react';
+import { FaDev, FaGithubSquare, FaLinkedin, FaGitlab } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <>
-      <div className='w-100 h[250px] bg-gray-200 p-10 flex justify-between items-center'>
-        <h1 className='text-3xl font-bold text-blue-500'>Dev-Faith</h1>
-        
-          <p>Copyright ❤️ © 2024 - All right reserved</p>
-          <a href="https://dev.to/faith_2024"><FaDev className='text-3xl'/> </a>
-          <a href="https://github.com/DevFaith"><FaGithubSquare className='text-3xl' /></a>
-          <a href="https://www.linkedin.com/in/faith-waithera-0a4b3329b/"><FaLinkedin className='text-3xl' /> </a>
-          <FaGitlab  className='text-3xl'/>
-        
+    <div className='w-full h-auto bg-gray-200 p-5 md:p-10 flex flex-col md:flex-row justify-between items-center'>
+      <h1 className='text-2xl md:text-3xl font-bold text-blue-500 mb-4 md:mb-0'>Dev-Faith</h1>
+      <p className='text-center mb-4 md:mb-0'>Copyright ❤️ © 2024 - All right reserved</p>
+      <div className='flex space-x-4'>
+        <a href="https://dev.to/faith_2024" className='text-2xl md:text-3xl'><FaDev /></a>
+        <a href="https://github.com/DevFaith" className='text-2xl md:text-3xl'><FaGithubSquare /></a>
+        <a href="https://www.linkedin.com/in/faith-waithera-0a4b3329b/" className='text-2xl md:text-3xl'><FaLinkedin /></a>
+        <a href="https://gitlab.com/DevFaith" className='text-2xl md:text-3xl'><FaGitlab /></a>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
